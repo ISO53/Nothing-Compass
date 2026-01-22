7823e65fbc592b06a6738c6e85449f5c43788d7a
iso53 <102249575+ISO53@users.noreply.github.com>
2026-01-22 23:45:39 +0300

    compass direction text are done

--- app/src/main/java/io/github/iso53/nothingcompass/view/CompassViewModel.java
+++ app/src/main/java/io/github/iso53/nothingcompass/view/CompassViewModel.java
@@ -12,7 +12,7 @@ import io.github.iso53.nothingcompass.model.LocationStatus;
 import io.github.iso53.nothingcompass.model.SensorAccuracy;
 
 public class CompassViewModel extends ViewModel {
-    private final MutableLiveData<Azimuth> azimuth = new MutableLiveData<>();
+    private final MutableLiveData<Azimuth> azimuth = new MutableLiveData<>(new Azimuth(0));
     private final MutableLiveData<SensorAccuracy> sensorAccuracy = new MutableLiveData<>(SensorAccuracy.NO_CONTACT);
     private final MutableLiveData<Boolean> trueNorth = new MutableLiveData<>(false);
     private final MutableLiveData<Boolean> hapticFeedback = new MutableLiveData<>(true);
