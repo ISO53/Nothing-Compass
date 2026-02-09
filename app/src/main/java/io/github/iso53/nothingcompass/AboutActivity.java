c1cc31d1ec00fa17e0e810b7b8ae0dbd0e6474fb
iso53 <102249575+ISO53@users.noreply.github.com>
2026-02-09 17:19:02 +0300

    changed the font family and style of Toolbars

--- app/src/main/java/io/github/iso53/nothingcompass/AboutActivity.java
+++ app/src/main/java/io/github/iso53/nothingcompass/AboutActivity.java
@@ -1,16 +1,17 @@
 package io.github.iso53.nothingcompass;
 
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.preference.PreferenceManager;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 import io.github.iso53.nothingcompass.preference.PreferenceConstants;
 
@@ -27,15 +28,7 @@ public class AboutActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_about);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.aboutToolbar), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
-            return insets;
-        });
-
-        findViewById(R.id.aboutToolbar).setOnClickListener(v -> finish());
-        ((androidx.appcompat.widget.Toolbar) findViewById(R.id.aboutToolbar))
-                .setNavigationOnClickListener(v -> finish());
+        setupToolbar();
 
         TextView versionText = findViewById(R.id.textVersion);
         try {
@@ -45,5 +38,18 @@ public class AboutActivity extends AppCompatActivity {
         } catch (Exception e) {
             versionText.setText(getString(R.string.about_version, "1.0"));
         }
+    }
+
+    private void setupToolbar() {
+        // Add back button
+        findViewById(R.id.aboutToolbar).setOnClickListener(v -> finish());
+        ((androidx.appcompat.widget.Toolbar) findViewById(R.id.aboutToolbar))
+                .setNavigationOnClickListener(v -> finish());
+
+        // Change the font of the title
+        CollapsingToolbarLayout collapsingToolbar = findViewById(R.id.collapseToolbar);
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.ntype82headline);
+        collapsingToolbar.setExpandedTitleTypeface(typeface);
+        collapsingToolbar.setCollapsedTitleTypeface(typeface);
     }
 }
